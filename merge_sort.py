42681019a0e00c0bc23cfebda798643892e84f95
jbbrokaw <jbrokaw@gmail.com>
2014-11-19 14:41:10 -0800

    Adds merge_sort_in_place, which runs slower but requires no memory

--- merge_sort.py
+++ merge_sort.py
@@ -27,6 +27,50 @@ def merge_sort(datalist):
     sorted_right = merge_sort(datalist[halfway:])
     return merge(sorted_left, sorted_right)
 
+
+def _shift_right(datalist, start, end):
+    """Shifts elements datalist[start] to datalist[end - 1] into slots
+    datalist[start + 1], datalist[end] without creating any copies.
+    Overwrites datalist[end] (which must be allocated)."""
+    index = end
+    while index > start:
+        datalist[index] = datalist[index - 1]
+        index -= 1
+
+
+def merge_in_place(datalist, start, end):
+    """Merge the two sorted sublists from start to halfway (right) and from
+    halfway to end - 1 (left) into one sorted list from start to end - 1"""
+    halfway = (end - start) // 2 + start
+    left_pointer = start
+    right_pointer = halfway
+    shifts = 0
+    while left_pointer < (halfway + shifts):
+        while (right_pointer < end) and \
+                (datalist[right_pointer] < datalist[left_pointer]):
+            tmp = datalist[right_pointer]
+            _shift_right(datalist, left_pointer, right_pointer)
+            datalist[left_pointer] = tmp
+            left_pointer += 1
+            right_pointer += 1
+            shifts += 1
+        left_pointer += 1
+
+
+def merge_sort_in_place(datalist, start=0, end=None):
+    """Merge sort in-place. Avoids making copies of the list but runs in
+    non-linearithmic quasilinear time O(n log2 n) (because of the shifts).
+    Runs much better on sorted lists (because no shifts needed)"""
+    if end is None:
+        end = len(datalist)
+    size = end - start
+    if size <= 1:
+        return
+    halfway = size // 2 + start
+    merge_sort_in_place(datalist, start, halfway)
+    merge_sort_in_place(datalist, halfway, end)
+    merge_in_place(datalist, start, end)
+
 if __name__ == '__main__':
     import timeit
     cases = {'ordered': 'testlist = range(%d)',
@@ -43,5 +87,17 @@ On average, filling & sorting an array of %d %s numbers takes %e s"""
             print pattern % (length, case, timeit.timeit(
                 "merge_sort(testlist)",
                 setup="from __main__ import merge_sort;" +
+                (cases[case] % length),
+                number=10) / 10.)
+
+    pattern = """
+On average, filling & sorting an array of %d %s numbers in place takes %e s"""
+
+    for case in cases:
+        for i in xrange(6, 13):
+            length = 2 ** i
+            print pattern % (length, case, timeit.timeit(
+                "merge_sort_in_place(testlist)",
+                setup="from __main__ import merge_sort_in_place;" +
                 (cases[case] % length),
                 number=10) / 10.)
--- test_merge_sort.py
+++ test_merge_sort.py
@@ -9,6 +9,7 @@ from __future__ import unicode_literals
 import pytest  # used for the exception testing
 
 from merge_sort import merge_sort, merge
+from merge_sort import merge_sort_in_place, merge_in_place, _shift_right
 
 
 def test_input():
@@ -34,6 +35,36 @@ def test_sorting():
     import random
     datalist = [random.randint(0, 1e6) for i in xrange(100)]
     datalist = merge_sort(datalist)
+
+    previous = datalist[0]
+    for i in datalist[1:]:
+        assert previous <= i
+        previous = i
+
+
+def test_shift_right():
+    testlist = [1, 2, 3, 4, 5, -1, 0]
+    end = len(testlist)
+    # Want to shift this right
+    _shift_right(testlist, 0, end - 1)
+    testlist[0] = 0
+    assert len(testlist) == end
+    assert testlist == [0, 1, 2, 3, 4, 5, -1]
+
+
+def test_merge_in_place():
+    testlist = [7, 12, 20, 4, 6, 22]
+    merge_in_place(testlist, 0, len(testlist))
+    assert testlist == [4, 6, 7, 12, 20, 22]
+    testlist = [7, 12, 20, 4, 6, 18]
+    merge_in_place(testlist, 0, len(testlist))
+    assert testlist == [4, 6, 7, 12, 18, 20]
+
+
+def test_merge_sort_in_place():
+    import random
+    datalist = [random.randint(0, 1e6) for i in xrange(100)]
+    merge_sort_in_place(datalist)
 
     previous = datalist[0]
     for i in datalist[1:]:
