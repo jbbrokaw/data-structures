f2e42ace4ed2063d29e18a216dbb2f6d99fadd34
jbbrokaw <jbrokaw@gmail.com>
2014-11-20 14:24:10 -0800

    Adds demo to radix_sort in name==main

--- radix_sort.py
+++ radix_sort.py
@@ -28,6 +28,7 @@ def radix_sort_int(intlist):
 
 
 def radix_sort_str(strlist):
+    """Sort a list of strings (in place) using radix sort. Case is ignored"""
     offset = ord('a') - 1  # We want a placeholder space before 'a', chr(96)
     max_length = 0
     for word in strlist:
@@ -47,3 +48,23 @@ def radix_sort_str(strlist):
             bucket[:] = []
 
     strlist[:] = [word.strip(chr(96)) for word in strlist]
+
+
+if __name__ == '__main__':
+    import timeit
+    cases = {'ordered': 'testlist = range(%d)',
+             'random': 'import random; ' +
+             'testlist = [random.randint(0, 1e6) for i in xrange(%d)]',
+             'reversed': 'testlist = range(%d, 0, -1)'}
+
+    pattern = """
+On average, filling & sorting an array of %d %s numbers takes %e s"""
+
+    for case in cases:
+        for i in xrange(6, 15):
+            length = 2 ** i
+            print pattern % (length, case, timeit.timeit(
+                "radix_sort_int(testlist)",
+                setup="from __main__ import radix_sort_int;" +
+                (cases[case] % length),
+                number=10) / 10.)
